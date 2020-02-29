4515324f09e6727f06d7c748cc380c60e7ee62b9
Gyan Vivek <Gyan.Vivek@ril.com>
2020-02-29 15:56:47 +0530

    added script to generate react func script

--- scripts/reactscript/gen_fun_component.js
+++ scripts/reactscript/gen_fun_component.js
@@ -1,4 +1,38 @@
-import fs from "fs";
+const fs = require("fs");
+const path = require("path");
 
-console.log("input argument"+process.argv);
-console.log("present dir name"+__dirname);
+//console.log("input argument"+process.argv);
+console.log("present dir name "+__dirname);
+
+let newFilename = process.argv[2]+".js";
+console.log("new file name :"+newFilename);
+let newfilenamePath = path.join(__dirname,newFilename);
+console.log("new file complete path :"+newfilenamePath);
+let templateFilePath =  process.argv[1];
+templateFilePath=path.dirname(templateFilePath);
+templateFilePath=path.join(templateFilePath,"templates","fun_comp.txt");
+console.log("temp file path is :"+templateFilePath);
+
+const transform = function(data){
+    //if(typeof data === "string"){
+       return data.toString().replace(/<name>/g,process.argv[2]);
+    //}
+    //return data;
+}
+
+
+fs.readFile(templateFilePath,(err,data)=>{  
+    if(!err){
+         console.log("data is:"+data);
+         fs.writeFile(newfilenamePath,transform(data),(err)=>{
+            console.log(err);
+            
+         })
+    };
+    
+    //console.log("")
+  })
+
+// fs.readFile("",(err,data)=>{
+//   fs.writeFile
+// })
