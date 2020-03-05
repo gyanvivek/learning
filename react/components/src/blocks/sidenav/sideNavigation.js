a352aece0414f8bd5300a30f5e7d87fd02629cc0
Gyan Vivek <gyanvivek123@gmail.com>
2020-03-05 23:17:17 +0530

    expanded list

--- react/components/src/blocks/sidenav/sideNavigation.js
+++ react/components/src/blocks/sidenav/sideNavigation.js
@@ -1,26 +1,46 @@
-import React from "react";
-import {Nav} from "react-bootstrap";
+import React, { useState } from "react";
+import { Nav ,Collapse } from "react-bootstrap";
+import ExpandDropdown from "./expandDropdown";
 
-const SideNavigation = ({paths})=>{
-    const [path,setPath] = React.useState("/home");
+const SideNavigation = ({ paths , value }) => {
+  
+   let defaultValue = { state:true}
+   defaultValue = typeof value === "undefined" ? defaultValue : {...defaultValue,...value};
 
-    const renderPath = (paths)=>{
-       return   paths.map((item,index)=>{
-            console.log(item);
-              if(Array.isArray(item)){
-                 return ( <Nav>{renderPath(item) }</Nav>);
+   let [collapseState ,setCollapseState] = useState(new Map());
 
-              }else{
-              return <Nav.Link {...item}>{item.title}</Nav.Link>
-              }
-          })
-    }
-    return (
-        <Nav defaultActiveKey="/home" className="flex-column">
-          {renderPath(paths)}
-        </Nav>
-    )
-
-}
+   const initCollapseState = function(level,paths){
+         if(!(typeof paths === "undefined")){
+            //if(Array.isArray(paths))
+         }
+   }
+   
+  //  React.useEffect(()=>{
+  //     if(!(typeof paths === "undefined"))
+  //      paths.array.forEach(element => {
+         
+  //      });
+  //  })
+   
+   const renderPath = paths => {
+    return paths.map((item, index) => {
+      console.log(item);
+      if (Array.isArray(item)) {
+      return <ExpandDropdown list={item}  collapse={false} renderItem={(item)=>{return(<div>{item.item}</div>)}}/>;
+      } else {
+        return <Nav.Link {...item}>{item.title}</Nav.Link>;
+      }
+    });
+  };
+  return (
+    <div  style={{width:200}}>
+    <Collapse in={defaultValue.state}>
+    <Nav defaultActiveKey="/home" className="flex-column">
+      {renderPath(paths)}
+    </Nav>
+    </Collapse>
+    </div>
+  );
+};
 
 export default SideNavigation;
--- react/components/src/views/playground/playground.js
+++ react/components/src/views/playground/playground.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import {Nav} from "react-bootstrap";
 import SideNavigation from "../../blocks/sidenav/sideNavigation";
+import {Button} from "react-bootstrap";
 
 const paths =  [{href:"home",title:"home"},
 {href:"home",title:"home"},
@@ -10,9 +11,11 @@ const paths =  [{href:"home",title:"home"},
 ];
 
 const Playground = ()=>{
+   const [navState,setNavState] = useState(true);
    return(
       <div>
-       <SideNavigation paths={paths}/>
+         <Button onClick={()=>setNavState(!navState)}> Toggle </Button>
+         <SideNavigation paths={paths} value={{state:navState}}/>
       </div>
  );
     
